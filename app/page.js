8414e95f9209c9fb83a0ea6365c360a9866bfdcc
SaChan <vltpcks@gmail.com>
2023-09-21 23:44:43 +0900

    connect mongodb

--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <main></main>;
+import { connectDB } from "@/util/database";
+
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+
+  console.log(result);
+
+  return (
+    <main>
+      <div>hello</div>
+    </main>
+  );
 }
