eba4bd6b097eec0da2668dc70448f9f6f0c4c8c1
SaChan <vltpcks@gmail.com>
2023-10-02 09:37:54 +0900

    aladin api error solve

--- app/commend/page.js
+++ app/commend/page.js
@@ -1,8 +1,18 @@
-import Sample from "./sample";
+"use client";
 
-export default async function Commend() {
-  const resposn = await fetch("http://localhost:3000/api/post/aladin");
-  console.log(resposn, "asdf");
+import { useEffect, useState } from "react";
+
+export default function Commend() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/post/aladin")
+      .then((res) => res.json())
+      .then((item) => {
+        setData(item);
+      });
+  }, []);
+
+  console.log(data);
   return (
     <div>
       <p>책 추천 사이트</p>
