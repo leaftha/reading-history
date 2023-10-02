e7aec0a614d1eb00855966a1b168ee157096cb6a
SaChan <vltpcks@gmail.com>
2023-10-02 20:21:42 +0900

    api data list

--- app/commend/page.js
+++ app/commend/page.js
@@ -8,14 +8,14 @@ export default function Commend() {
     fetch("http://localhost:3000/api/post/aladin")
       .then((res) => res.json())
       .then((item) => {
-        setData(item);
+        setData(item.item);
       });
   }, []);
 
-  console.log(data);
   return (
     <div>
       <p>책 추천 사이트</p>
+      {data ? data.map((item, idx) => <p key={idx}>{item.title}</p>) : ""}
       <button>button</button>
     </div>
   );
--- pages/api/post/aladin.js
+++ pages/api/post/aladin.js
@@ -5,9 +5,15 @@ export default async function handler(req, res) {
     const q = await axios.get(
       "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttbvltpcks1846001&Query=aladdin&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20070901"
     );
+
+    //db 데이터
     const a = q.data;
+
+    //json 악성 예외 처리
     const c = a.replaceAll(`\\'`, "");
     const d = c.replaceAll(`;`, "");
+
+    //json 파싱
     const b = JSON.parse(d);
     res.status(200).json(b);
   } catch (err) {
