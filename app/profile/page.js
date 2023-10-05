e9fbb39afeb246530d68870d16ee8db40d461c36
leaftha <vltpcks@gmail.com>
2023-10-05 13:05:14 +0900

    tier system

--- app/profile/page.js
+++ app/profile/page.js
@@ -13,6 +13,18 @@ export default async function Profile() {
     const db = client.db('readingHistory');
 
     let result = await db.collection('user_cred').find({ email: session.user.email }).toArray();
+    let reviews = await db.collection('reviews').find({ email: session.user.email }).toArray();
+    let tier = '';
+
+    if (reviews.length < 10) {
+        tier = '뽀시래기';
+    } else if (reviews.length < 20) {
+        tier = '갱얼쥐';
+    } else if (reviews.length < 30) {
+        tier = '지식인';
+    } else if (reviews.length < 40) {
+        tier = '도서왕';
+    }
 
     let tastes = [...result[0].taste];
     return (
@@ -20,6 +32,8 @@ export default async function Profile() {
             <p>profile</p>
             <p>{session.user.name}</p>
             <p>{session.user.email}</p>
+            <p>{reviews.length}</p>
+            <p>{tier}</p>
             <div>
                 <h1>현재 취양</h1>
                 {tastes ? tastes.map((item, idx) => <p key={idx}>{item}</p>) : '현재 없음'}
