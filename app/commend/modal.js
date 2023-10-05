9f1d022c3d659d3a307cc46072fafc937efda7eb
leaftha <vltpcks@gmail.com>
2023-10-05 13:15:58 +0900

    cover img

--- app/commend/modal.js
+++ app/commend/modal.js
@@ -1,20 +1,24 @@
-import Comment from "./comment";
+import Comment from './comment';
+import classes from './modal.module.css';
 
 export default function Modal({ item, setModal, session }) {
-  const closeModel = () => {
-    setModal(false);
-  };
+    const closeModel = () => {
+        setModal(false);
+    };
 
-  // console.log(item);
+    console.log(item);
 
-  return (
-    <div>
-      <button onClick={closeModel}>X</button>
-      <p>{item.title}</p>
-      <p>{item.author}</p>
-      <p>{item.categoryName}</p>
-      <p>{item.description}</p>
-      <Comment session={session} id={item.itemId} />
-    </div>
-  );
+    return (
+        <div className={classes.modal}>
+            <div className={classes.main}>
+                <button onClick={closeModel}>X</button>
+                <img src={item.cover} alt="coverImg" />
+                <p>{item.title}</p>
+                <p>{item.author}</p>
+                <p>{item.categoryName}</p>
+                <p>{item.description}</p>
+                <Comment session={session} id={item.itemId} />
+            </div>
+        </div>
+    );
 }
