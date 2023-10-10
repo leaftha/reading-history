cfba38946e3e85be6a1f0841e6f5453feea732a7
leaftha <vltpcks@gmail.com>
2023-10-10 10:50:31 +0900

    review pagin

--- app/reviews/page.js
+++ app/reviews/page.js
@@ -1,37 +1,31 @@
-import { connectDB } from "@/util/database";
-import { authOptions } from "@/pages/api/auth/[...nextauth].js";
-import { getServerSession } from "next-auth";
-import Link from "next/link";
-import NotAuth from "../notauth";
+import { connectDB } from '@/util/database';
+import { authOptions } from '@/pages/api/auth/[...nextauth].js';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import NotAuth from '../notauth';
+import List from './list';
 
 export default async function Reviews() {
-  let session = await getServerSession(authOptions);
-  if (!session) {
-    return <NotAuth />;
-  }
-  const client = await connectDB;
-  const db = client.db("readingHistory");
+    let session = await getServerSession(authOptions);
+    if (!session) {
+        return <NotAuth />;
+    }
+    const client = await connectDB;
+    const db = client.db('readingHistory');
 
-  let result = await db
-    .collection("reviews")
-    .find({ email: session.user.email })
-    .toArray();
-  result = result.map((a) => {
-    a._id = a._id.toString();
-    return a;
-  });
+    let result = await db.collection('reviews').find({ email: session.user.email }).toArray();
+    result = result.map((a) => {
+        a._id = a._id.toString();
+        return a;
+    });
 
-  return (
-    <div>
-      <Link href="writing">글 작성</Link>
-      <Link href="reviewlist">다른 사람 서평들</Link>
-      <p>서평 수 : {result.length}</p>
-      <p>reviews</p>
-      {result.map((item, i) => (
-        <Link key={i} href={`/detail/${result[i]._id}`}>
-          {item.title}
-        </Link>
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            <Link href="writing">글 작성</Link>
+            <Link href="reviewlist">다른 사람 서평들</Link>
+            <p>서평 수 : {result.length}</p>
+            <p>reviews</p>
+            <List result={result} />
+        </div>
+    );
 }
