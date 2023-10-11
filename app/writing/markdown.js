16982b31550be367b4d126e98c81e7b04266f37f
leaftha <vltpcks@gmail.com>
2023-10-11 10:26:43 +0900

    private select error fix

--- app/writing/markdown.js
+++ app/writing/markdown.js
@@ -1,38 +1,39 @@
-"use client";
+'use client';
 
-import MDEditor from "@uiw/react-md-editor";
-import { useState } from "react";
+import MDEditor from '@uiw/react-md-editor';
+import { useEffect, useState } from 'react';
 
 export default function Markdown({ session }) {
-  const [value, setValue] = useState("");
-  const [privates, setPrivates] = useState(false);
-  const [privateValue, setPrivateValue] = useState(`${privates}`);
-  return (
-    <form action="/api/post/writing" method="POST">
-      <input name="email" defaultValue={session.user.email} />
-      <label>제목</label>
-      <input name="title" />
-      <select
-        onChange={() => {
-          setPrivateValue(!privates);
-          setPrivates(!privates);
-        }}
-        value={privates}
-      >
-        <option value={false} key="false">
-          비공개
-        </option>
-        <option value={true} key="true">
-          공개
-        </option>
-      </select>
-      <input name="private" defaultValue={privateValue} />
+    const [value, setValue] = useState('');
+    const [privates, setPrivates] = useState(false);
 
-      <div data-color-mode="light">
-        <MDEditor height={200} value={value} onChange={setValue} />
-      </div>
-      <textarea name="main" defaultValue={value}></textarea>
-      <button type="submit">버튼</button>
-    </form>
-  );
+    useEffect(() => {}, [privates]);
+
+    return (
+        <form action="/api/post/writing" method="POST">
+            <input name="email" defaultValue={session.user.email} readOnly={true} />
+            <label>제목</label>
+            <input name="title" />
+            <select
+                onChange={() => {
+                    setPrivates(!privates);
+                }}
+                value={privates}
+            >
+                <option value={false} key="false">
+                    비공개
+                </option>
+                <option value={true} key="true">
+                    공개
+                </option>
+            </select>
+            <input name="private" value={privates} readOnly={true} />
+
+            <div data-color-mode="light">
+                <MDEditor height={200} value={value} onChange={setValue} />
+            </div>
+            <textarea name="main" defaultValue={value}></textarea>
+            <button type="submit">버튼</button>
+        </form>
+    );
 }
