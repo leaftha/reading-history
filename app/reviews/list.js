41dcf22da2db322cb831026d7f21700dfe420f59
SaChan <vltpcks@gmail.com>
2023-11-19 00:05:30 +0900

    review list desgin

--- app/reviews/list.js
+++ app/reviews/list.js
@@ -1,39 +1,42 @@
-'use client';
-import Link from 'next/link';
-import Paging from './pagin';
-import { useEffect, useState } from 'react';
+"use client";
+import Link from "next/link";
+import Paging from "./pagin";
+import classes from "./list.module.css";
+import { useEffect, useState } from "react";
 
 export default function List({ result }) {
-    const [products, setProducts] = useState([...result]); // 리스트에 나타낼 아이템들
-    const [count, setCount] = useState(0); // 아이템 총 개수
-    const [currentPage, setCurrentPage] = useState(1); // 현재 페이지. default 값으로 1
-    const [postPerPage] = useState(5); // 한 페이지에 보여질 아이템 수
-    const [indexOfLastPost, setIndexOfLastPost] = useState(0); // 현재 페이지의 마지막 아이템 인덱스
-    const [indexOfFirstPost, setIndexOfFirstPost] = useState(0); // 현재 페이지의 첫번째 아이템 인덱스
-    const [currentPosts, setCurrentPosts] = useState(0); // 현재 페이지에서 보여지는 아이템들
+  const [products, setProducts] = useState([...result]); // 리스트에 나타낼 아이템들
+  const [count, setCount] = useState(0); // 아이템 총 개수
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지. default 값으로 1
+  const [postPerPage] = useState(10); // 한 페이지에 보여질 아이템 수
+  const [indexOfLastPost, setIndexOfLastPost] = useState(0); // 현재 페이지의 마지막 아이템 인덱스
+  const [indexOfFirstPost, setIndexOfFirstPost] = useState(0); // 현재 페이지의 첫번째 아이템 인덱스
+  const [currentPosts, setCurrentPosts] = useState(0); // 현재 페이지에서 보여지는 아이템들
 
-    useEffect(() => {
-        setCount(products.length);
-        setIndexOfLastPost(currentPage * postPerPage);
-        setIndexOfFirstPost(indexOfLastPost - postPerPage);
-        setCurrentPosts(products.slice(indexOfFirstPost, indexOfLastPost));
-    }, [currentPage, indexOfLastPost, indexOfFirstPost, products, postPerPage]);
+  useEffect(() => {
+    setCount(products.length);
+    setIndexOfLastPost(currentPage * postPerPage);
+    setIndexOfFirstPost(indexOfLastPost - postPerPage);
+    setCurrentPosts(products.slice(indexOfFirstPost, indexOfLastPost));
+  }, [currentPage, indexOfLastPost, indexOfFirstPost, products, postPerPage]);
 
-    const setPage = (error) => {
-        setCurrentPage(error);
-    };
-    return (
-        <div>
-            {currentPosts && products.length > 0 ? (
-                currentPosts.map((item, idx) => (
-                    <Link key={idx} href={`/detail/${item._id}`}>
-                        {item.title}
-                    </Link>
-                ))
-            ) : (
-                <div> No posts.</div>
-            )}
-            <Paging page={currentPage} count={count} setPage={setPage} />
-        </div>
-    );
+  const setPage = (error) => {
+    setCurrentPage(error);
+  };
+  return (
+    <div className={classes.main}>
+      {currentPosts && products.length > 0 ? (
+        currentPosts.map((item, idx) => (
+          <Link className={classes.link} key={idx} href={`/detail/${item._id}`}>
+            {item.title}
+          </Link>
+        ))
+      ) : (
+        <div className={classes.noPost}> No posts.</div>
+      )}
+      <div className={classes.Paging}>
+        <Paging page={currentPage} count={count} setPage={setPage} />
+      </div>
+    </div>
+  );
 }
--- app/reviews/page.js
+++ app/reviews/page.js
@@ -1,33 +1,39 @@
-import { connectDB } from '@/util/database';
-import { authOptions } from '@/pages/api/auth/[...nextauth].js';
-import { getServerSession } from 'next-auth';
-import "./page.review.model.css"
-import Link from 'next/link';
-import NotAuth from '../notauth';
-import List from './list';
-;
+import { connectDB } from "@/util/database";
+import { authOptions } from "@/pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import NotAuth from "../notauth";
+import List from "./list";
+import classes from "./page.module.css";
 
 export default async function Reviews() {
-    let session = await getServerSession(authOptions);
-    if (!session) {
-        return <NotAuth />;
-    }
-    const client = await connectDB;
-    const db = client.db('readingHistory');
+  let session = await getServerSession(authOptions);
+  if (!session) {
+    return <NotAuth />;
+  }
+  const client = await connectDB;
+  const db = client.db("readingHistory");
 
-    let result = await db.collection('reviews').find({ email: session.user.email }).toArray();
-    result = result.map((a) => {
-        a._id = a._id.toString();
-        return a;
-    });
+  let result = await db
+    .collection("reviews")
+    .find({ email: session.user.email })
+    .toArray();
+  result = result.map((a) => {
+    a._id = a._id.toString();
+    return a;
+  });
 
-    return (
-        <div>
-            <Link href="writing">글 작성</Link>
-            <Link href="reviewlist">다른 사람 서평들</Link>
-            <p>서평 수 : {result.length}</p>
-            <p>reviews</p>
-            <List result={result} />
-        </div>
-    );
+  return (
+    <div className={classes.main}>
+      <Link className={classes.wrting} href="writing">
+        글 작성
+      </Link>
+      <Link className={classes.another} href="reviewlist">
+        다른 사람 서평들
+      </Link>
+      <p className={classes.num}>서평 수 : {result.length}</p>
+      <p className={classes.listTitle}>reviews</p>
+      <List result={result} />
+    </div>
+  );
 }
