466c8a51ae9eba8c885eb0bec5ea6a7276ec902f
SaChan <vltpcks@gmail.com>
2023-11-19 21:40:26 +0900

    writing page desgin

--- app/writing/markdown.js
+++ app/writing/markdown.js
@@ -1,41 +1,64 @@
-'use client';
+"use client";
 
-import MDEditor from '@uiw/react-md-editor';
-import { useEffect, useState } from 'react';
-import "./markdown.model.css";
+import MDEditor from "@uiw/react-md-editor";
+import { useEffect, useState } from "react";
+import classes from "./markdown.module.css";
 
 export default function Markdown({ session }) {
-    const [value, setValue] = useState('');
-    const [privates, setPrivates] = useState(false);
+  const [value, setValue] = useState("");
+  const [privates, setPrivates] = useState(false);
 
-    useEffect(() => {}, [privates]);
+  useEffect(() => {}, [privates]);
 
-    return (
-        <form action="/api/post/writing" method="POST">
-            <input name="email" defaultValue={session.user.email} readOnly={true} />
-            <label>제목</label>
-            <input name="title" />
-            <select
-                onChange={() => {
-                    setPrivates(!privates);
-                }}
-                value={privates}
-            >
-                <option value={false} key="false">
-                    비공개
-                </option>
-                <option value={true} key="true">
-                    공개
-                </option>
-            </select>
-            <input name="private" value={privates} readOnly={true} />
+  return (
+    <form className={classes.Form} action="/api/post/writing" method="POST">
+      <input
+        className={classes.none}
+        name="email"
+        defaultValue={session.user.email}
+        readOnly={true}
+      />
+      <div className={classes.content}>
+        {/* <label className={classes.inputLable}>제목</label> */}
+        <input
+          placeholder={"제목을 입력하세요."}
+          name="title"
+          className={classes.inputTitle}
+        />
+        <div className={classes.line}></div>
+        <select
+          className={classes.selector}
+          onChange={() => {
+            setPrivates(!privates);
+          }}
+          value={privates}
+        >
+          <option value={false} key="false">
+            비공개
+          </option>
+          <option value={true} key="true">
+            공개
+          </option>
+        </select>
+      </div>
+      <input
+        className={classes.none}
+        name="private"
+        value={privates}
+        readOnly={true}
+      />
 
-            <div data-color-mode="light">
-                <MDEditor height={200} value={value} onChange={setValue} />
-            </div>
-            <textarea name="main" defaultValue={value}></textarea>
-            <button type="submit">작성하기</button> 
-            
-        </form>
-    );
+      <div className={classes.markdown} data-color-mode="light">
+        <MDEditor height={600} value={value} onChange={setValue} />
+      </div>
+      <textarea
+        className={classes.none}
+        name="main"
+        defaultValue={value}
+      ></textarea>
+      <button className={classes.btn} type="submit">
+        작성하기
+      </button>
+    </form>
+  );
 }
--- app/writing/page.js
+++ app/writing/page.js
@@ -2,6 +2,7 @@ import { authOptions } from "@/pages/api/auth/[...nextauth].js";
 import { getServerSession } from "next-auth";
 import Markdown from "./markdown";
 import NotAuth from "../notauth";
+import classes from "./page.module.css";
 
 export default async function Writing() {
   let session = await getServerSession(authOptions);
@@ -9,8 +10,8 @@ export default async function Writing() {
     return <NotAuth />;
   }
   return (
-    <div>
-      <h4>글작성</h4>
+    <div className={classes.main}>
+      <h4 className={classes.title}>글작성</h4>
       <Markdown session={session} />
     </div>
   );
