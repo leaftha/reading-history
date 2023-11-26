6b17124e432af8cb9ff63b2113422441a6dc4b0d
SaChan <vltpcks@gmail.com>
2023-11-26 17:47:59 +0900

    debate edit page desgin

--- app/Debateedit/[id]/markdown.js
+++ app/Debateedit/[id]/markdown.js
@@ -2,19 +2,33 @@
 
 import MDEditor from "@uiw/react-md-editor";
 import { useState } from "react";
+import classes from "./markdown.module.css";
 
 export default function Markdown({ result }) {
   const [value, setValue] = useState(result.main);
   return (
     <form action="/api/debate/edit" method="POST">
-      <input name="id" defaultValue={result._id} />
-      <label>제목</label>
-      <input name="title" defaultValue={result.title} />
-      <div data-color-mode="light">
-        <MDEditor height={200} value={value} onChange={setValue} />
+      <input className={classes.none} name="id" defaultValue={result._id} />
+
+      <div className={classes.content}>
+        <input
+          className={classes.inputTitle}
+          name="title"
+          defaultValue={result.title}
+        />
       </div>
-      <textarea name="main" value={value} onChange={setValue}></textarea>
-      <button type="submit">버튼</button>
+      <div className={classes.markdown} data-color-mode="light">
+        <MDEditor height={600} value={value} onChange={setValue} />
+      </div>
+      <textarea
+        className={classes.none}
+        name="main"
+        value={value}
+        onChange={setValue}
+      ></textarea>
+      <button className={classes.btn} type="submit">
+        수정
+      </button>
     </form>
   );
 }
--- app/Debateedit/[id]/page.js
+++ app/Debateedit/[id]/page.js
@@ -2,7 +2,7 @@ import { ObjectId } from "mongodb";
 import { authOptions } from "@/pages/api/auth/[...nextauth].js";
 import { getServerSession } from "next-auth";
 import { connectDB } from "@/util/database.js";
-
+import classes from "./page.module.css";
 import MarkdownEdit from "./markdown";
 
 export default async function Edit(props) {
@@ -14,15 +14,15 @@ export default async function Edit(props) {
 
   if (session.user.email != result.email) {
     return (
-      <div>
-        <p>작성자가 아닙니다.</p>
+      <div className={classes.main}>
+        <h1 className={classes.title}>작성자가 아닙니다.</h1>
       </div>
     );
   }
 
   return (
-    <div>
-      <h4>글 수정</h4>
+    <div className={classes.main}>
+      <h4 className={classes.title}>글 수정</h4>
       <MarkdownEdit session={session} result={result} />
     </div>
   );
